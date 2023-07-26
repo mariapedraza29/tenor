18ab5e31e5efd9dc44a092bce83685230a0476ea
mariapedraza29 <pedrazamariia@gmaul.com>
2023-07-25 21:31:49 -0500

    actualizacion

--- app.js
+++ app.js
@@ -1,33 +1,67 @@
- const url='https://tenor.googleapis.com/v2/search'
- let busqueda='?q='
- let q = '';
- let key = '&key=AIzaSyDErkesndjoYQ83QBwc8qxxcSDYPMrMNIo'
-const limite= '&limit=40';
 
-let urlComplete ='';
-urlComplete = url + busqueda + q+ key+limite;
+const url = "https://api.giphy.com/v1/gifs/search";
+const key = "&api_key=LFbuRCn1Uo6KEX69wOVNG6yrTx4OzuMY";
+const limit = "&limit=30";
+let search = "?q=";
+let q = "";
+
+let urlComp = "";
+
+urlComp = url + search + q + key + limit;
+
 const btn = document.getElementById('btn');
-
-btn.onclick=()=>{
-
-    document.getElementById('portafolio');
-    q= document.getElementById('finder').value;
-    urlComplete
-    getData();
+btn.onclick = () => {
+    document.getElementById('portfolio').innerHTML = "";
+    document.getElementById('trending').innerHTML="";
+    q = document.getElementById('search').value;
+    urlComp = url + search + q + key + limit;
+    getDataSearch();
 }
-const getData = async ()=>{
-    await fetch(urlComplete).then((response) => {
-        return response.json();
-    }).then((tenor) =>{
-        console.log(tenor);
-    
 
-    for( let i=0; i<tenor.data.length; i++){
-        const gif= document.createElement('img');
-        gif.src= tenor.data[i].images["original"].url;
-        gif.className="mb-3"
-        document.getElementById("portafolio").appendChild(gif)
-    }
-})
+const btnTrending = document.getElementById('trending');
+btnTrending.onclick = () => {
+    document.getElementById('portfolio').innerHTML = "";
+    getData(); 
 }
-getData(); 
+
+const getDataSearch = async () => {
+    await fetch(urlComp)
+        .then((response) => response.json())
+        .then((giphy) => {
+            console.log(giphy);
+
+            for (let index = 0; index < giphy.data.length; index++) {
+                const gif = document.createElement('img');
+
+                gif.src = giphy.data[index].images["original"].url;
+                gif.className = "gifs_search";
+                document.getElementById("portfolio").appendChild(gif);
+            }
+        })
+        .catch((error) => {
+            console.error('Error fetching gifs:', error);
+        });
+}
+
+const trending = "https://api.giphy.com/v1/gifs/trending?api_key=LFbuRCn1Uo6KEX69wOVNG6yrTx4OzuMY&limit=30";
+
+const getData = async () => {
+    await fetch(trending)
+        .then((response) => response.json())
+        .then((giphy) => {
+            console.log(giphy);
+
+            for (let index = 0; index < giphy.data.length; index++) {
+                const gif = document.createElement('img');
+
+                gif.src = giphy.data[index].images["original"].url;
+                gif.className = "gifs_trending";
+                document.getElementById("portfolio").appendChild(gif);
+            }
+        })
+        .catch((error) => {
+            console.error('Error fetching:', error);
+        });
+}
+
+getData();
